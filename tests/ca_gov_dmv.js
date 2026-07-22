053ea7522d692ed466f89440a7d70375e8a327ef
somebodysmokein <venky88r@gmail.com>
2026-07-22 16:58:49 +0530

    updated ca gov file

--- tests/ca_gov_dmv.js
+++ tests/ca_gov_dmv.js
@@ -17,13 +17,20 @@ test("Navigate ca.gov Departments to DMV and verify homepage", async ({
   await departmentsLink.evaluate((el) => el.click());
   await expect(page).toHaveURL(/\/departments\/?$/);
 
+  const dept= await page.locator("//a[text()='View all departments']")
+  await dept.click();
+
+  const edd = page.locator("//a[@href='/departments/229/']");
+  await edd.click();
+
+
   // 3. Open the Department of Motor Vehicles (DMV) page
-  const dmvDeptLink = page
-    .locator("a.link-grid", { hasText: "Department of Motor Vehicles" })
-    .first();
-  await expect(dmvDeptLink).toBeVisible({ timeout: 15000 });
-  await dmvDeptLink.click();
-  await expect(page).toHaveURL(/\/departments\/299\/?/);
+  // const dmvDeptLink = page
+  //   .locator("a.link-grid", { hasText: "Contractors State License Board" })
+  //   .first();
+  // await expect(dmvDeptLink).toBeVisible({ timeout: 15000 });
+  // await dmvDeptLink.click();
+  await expect(page).toHaveURL(/\/departments\/229\/?/);
 
   // 4. Click the button that opens the official DMV website
   const departmentWebsiteBtn = page.getByRole("link", {
@@ -34,9 +41,23 @@ test("Navigate ca.gov Departments to DMV and verify homepage", async ({
 
   // 5. Verify the DMV site loaded via the help heading.
   // Live site heading text is "How can we help?"
-  await expect(page).toHaveURL(/dmv\.ca\.gov/i, { timeout: 30000 });
-  const helpLabel = page.getByRole("heading", {
-    name: /How can (we|I) help/i,
-  });
-  await expect(helpLabel).toBeVisible({ timeout: 30000 });
+  await expect(page).toHaveURL(/calepa\.ca\.gov/i, { timeout: 30000 });
+  // const helpLabel = page.getByRole("heading", {
+  //   name: /How can (we|I) help/i,
+  // });
+
+  // const calendarBtn= page.locator("#eventCalendar1");
+  // await expect(calendarBtn).toBeVisible({ timeout: 30000 });
+
+  // calendarBtn.click();
+
+  // const disasterlbl = page.locator(".add_icon_blue_arrow_down");
+  // await expect(disasterlbl).toBeVisible({ timeout: 30000 });
+
+
+  const popularLinks = page.locator("//span[text()='Popular links']");
+  await expect(page).toBeVisible();
+
+  
+
 });
